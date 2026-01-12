99b0d59e5a26ced15aaa5f44debffe212dab635b
KaikeMioranza <kaikemioranza@hotmail.com>
2026-01-12 18:07:08 -0300

    Correção de bug Flyway 1.3.2

--- src/main/java/com/back_end/Quizz/repository/QuizRepository.java
+++ src/main/java/com/back_end/Quizz/repository/QuizRepository.java
@@ -22,35 +22,34 @@ public interface QuizRepository extends JpaRepository<Quiz, Long> {
     @Query( value =
         """
             SELECT
-                p.ordem AS id,
-                p.enunciado,
-                q.categoria,
-            	p.tempo_limite_segundos,
-                MAX(CASE WHEN o.ordem = 1 THEN o.descricao END) AS A,
-                MAX(CASE WHEN o.ordem = 2 THEN o.descricao END) AS B,
-                MAX(CASE WHEN o.ordem = 3 THEN o.descricao END) AS C,
-                MAX(CASE WHEN o.ordem = 4 THEN o.descricao END) AS D,
-            
-                MAX(
-                    CASE\s
-                        WHEN o.correta = 1 AND o.ordem = 1 THEN 'A'
-                        WHEN o.correta = 1 AND o.ordem = 2 THEN 'B'
-                        WHEN o.correta = 1 AND o.ordem = 3 THEN 'C'
-                        WHEN o.correta = 1 AND o.ordem = 4 THEN 'D'
-                    END
-                ) AS correta
-            
-            FROM pergunta p
-            JOIN opcao_resposta o ON o.pergunta_id = p.id
-            JOIN quiz q ON q.id = p.quiz_id
-            
-            WHERE
-                q.data_inicio <= NOW()
-                AND (q.data_fim IS NULL OR q.data_fim >= NOW())
-            
-            GROUP BY
-                p.id,
-                p.enunciado;
+               p.ordem AS pergunta_id,
+               p.enunciado,
+               p.tempo_limite_segundos,
+               MAX(CASE WHEN o.ordem = 1 THEN o.descricao END) AS A,
+               MAX(CASE WHEN o.ordem = 2 THEN o.descricao END) AS B,
+               MAX(CASE WHEN o.ordem = 3 THEN o.descricao END) AS C,
+               MAX(CASE WHEN o.ordem = 4 THEN o.descricao END) AS D,
+       
+               MAX(
+                   CASE\s
+                       WHEN o.correta = 1 AND o.ordem = 1 THEN 'A'
+                       WHEN o.correta = 1 AND o.ordem = 2 THEN 'B'
+                       WHEN o.correta = 1 AND o.ordem = 3 THEN 'C'
+                       WHEN o.correta = 1 AND o.ordem = 4 THEN 'D'
+                   END
+               ) AS correta
+       
+           FROM pergunta p
+           JOIN opcao_resposta o ON o.pergunta_id = p.id
+           JOIN quiz q ON q.id = p.quiz_id
+       
+           WHERE
+               q.data_inicio <= NOW()
+               AND (q.data_fim IS NULL OR q.data_fim >= NOW())
+       
+           GROUP BY
+               p.id,
+               p.enunciado;
         """,
         nativeQuery = true
     )
--- src/test/java/com/back_end/Quizz/QuizzApplicationTests.java
+++ src/test/java/com/back_end/Quizz/QuizzApplicationTests.java
@@ -1,7 +1,9 @@
 package com.back_end.Quizz;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 class QuizzApplicationTests {
